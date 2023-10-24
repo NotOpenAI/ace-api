8b3785d2d70121de4c1af66ce5ce6ba620bdc735
qnd5128 <qnd5128@rit.edu>
2023-10-24 15:15:13 -0400

    initial some starter code for postgres

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
@@ -6,3 +6,11 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get(
+    "/init_tables",
+    status_code=status.HTTP_200_OK,
+    name="init_tables"
+)
+async def init_tables():
+    create_tables()
