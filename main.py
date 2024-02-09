d91e99554167815e526ab6c16857b32736135eb2
Brandon Montijo <bcm8504@rit.edu>
2024-02-08 21:52:23 -0500

    Add CORS Middleware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from endpoints import user, role, customer, bid, project
 
 app = FastAPI()
@@ -7,6 +9,19 @@ app.include_router(role.router)
 app.include_router(customer.router)
 app.include_router(bid.router)
 app.include_router(project.router)
+
+origins = [
+    "http://localhost:5173",
+    # TODO - add deployed app url to allow traffic from it
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
