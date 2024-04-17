6905323f37c924ca3e4d35a5e83b18ad82aedb45
qnd5128 <qnd5128@rit.edu>
2024-04-17 15:08:43 -0400

    error handling

--- endpoints/ai.py
+++ endpoints/ai.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, make_scorer
+from sklearn.model_selection import train_test_split, cross_val_score
 from sqlalchemy.orm import Session
 import numpy as np
 from crud import ai, bid
@@ -17,15 +17,16 @@ router = APIRouter(prefix="/ai", tags=["ai"])
 @router.get("/{bid_id}", response_model=SuccessResponse[float])
 async def get_bid(
     bid_id: int,
-    # current_user: Annotated[User, Depends(security.get_current_user)],
+    current_user: Annotated[User, Depends(security.get_current_user)],
     db: Session = Depends(deps.get_db),
 ):
     bid_obj = bid.get_by_id(bid_id, db)
     if not bid_obj:
         raise HTTPException(404, "Bid not found")
 
-    bids = ai.get_bid_data_for_prediction(db)
+    bids = ai.get_bid_datar_prediction(db)
     data, original_contract = data_to_df(bid_id, bids)
+
     data_cleaned = clean_data(bid_id, data)
 
     current_bid_data = data_cleaned[data_cleaned["id"] == bid_id]
@@ -50,7 +51,6 @@ def data_to_df(bid_id, bids):
     data = []
     original_contract = 0
     for bid in bids:
-        # exclude bid predicting
         if bid.id == bid_id:
             original_contract = (
                 bid.original_contract
@@ -58,7 +58,9 @@ def data_to_df(bid_id, bids):
                 else original_contract
             )
         row = {
-            column.name: getattr(bid, column.name) for column in bid.__table__.columns
+            column.name: getattr(bid, column.name)
+            for column in bid.__table__.columns
+            if hasattr(bid, column.name)
         }
         for attribute in bid.attributes:
             attribute_name = attribute.type.name
@@ -76,7 +78,7 @@ def data_to_df(bid_id, bids):
 
 def clean_data(bid_id, data):
     # choose columns with at least 40% of data
-    threshold = 0.4 * len(data)
+    threshold = 0.5 * len(data)
     data_cleaned = data.dropna(thresh=threshold, axis=1)
 
     columns_to_drop = ["name", "created_at", "comments", "desired_margin"]
@@ -93,24 +95,26 @@ def clean_data(bid_id, data):
 def train_model(data_cleaned):
     X = data_cleaned.drop(columns=["id", "final_cost"])
     y = data_cleaned["final_cost"]
+    random_forest_model = RandomForestRegressor(random_state=42)
 
     # Split the data
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
+        X, y, test_size=0.1, random_state=42
     )
 
     # Train the model
-    random_forest_model = RandomForestRegressor(random_state=42)
     random_forest_model.fit(X_train, y_train)
+
     return X_test, random_forest_model, y_test
 
 
 def predict_margin(current_bid_data, model, original_contract):
     if current_bid_data.isnull().values.any():
-        # print(current_bid_data)
         raise HTTPException(400, "Not enough bid information to predict.")
+    if original_contract == 0:
+        predicted_margin = 0
+        return predicted_margin
     predicted_cost = model.predict(current_bid_data)
-    # print(f"predicted_cost: {predicted_cost}, original_contract: {original_contract}")
     predicted_margin = 1 - (float(predicted_cost) / float(original_contract))
     return predicted_margin
 
